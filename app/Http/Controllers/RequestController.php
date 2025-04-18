807b152f687cc634274c9cae384cfe9a376aa4b2
Hazem <hazem.ismail@hotmail.com~>
2025-04-18 12:56:51 +0300

    make the app ready for replacement_id

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -8,21 +8,22 @@ use App\Models\Mission;
 use App\Models\Request as RequestModel;
 use App\Http\Resources\PermissionResource;
 use App\Http\Resources\MissionResource;
-
+use App\Models\User;
 
 class RequestController extends Controller
 {
     public function index()
     {
         abort_if(!request()->user()->can('viewAny', RequestModel::class), 403);
-        $users = auth()->user()->children()->orderBy('name')->get();
+
+        $users = User::whereIn('id', $this->getChildrenIds())->orderBy('name')->get();
         return view('pages.requests.index', compact('users'));
     }
 
     public function getCounts()
     {
-        $permissionsCount = Permission::whereIn('user_id', auth()->user()->children()->pluck('id'))->where('status', 'pending')->count();
-        $missionsCount = Mission::whereIn('user_id', auth()->user()->children()->pluck('id'))->where('status', 'pending')->count();
+        $permissionsCount = Permission::whereIn('user_id', $this->getChildrenIds())->where('status', 'pending')->count();
+        $missionsCount = Mission::whereIn('user_id', $this->getChildrenIds())->where('status', 'pending')->count();
         return response()->json([
             'permissionsCount' => $permissionsCount,
             'missionsCount' => $missionsCount
@@ -30,11 +31,24 @@ class RequestController extends Controller
     }
 
 
+    private function getChildrenIds(){
+
+        $supervisors = User::where('replacement_id', auth()->user()->id)
+            ->orWhere('id', auth()->user()->id)
+            ->get();
+        $childrenIds = [];
+        foreach ($supervisors as $supervisor) {
+            array_push($childrenIds, $supervisor->children()->pluck('id'));
+        }
+        return collect($childrenIds)->flatten()->values()->all();
+    }
+
 
     public function missions(Request $request)
     {
+
         $missions = Mission::query()
-            ->whereIn('user_id', auth()->user()->children()->pluck('id'))
+            ->whereIn('user_id', $this->getChildrenIds())
             ->latest()
             ->with('user')
             ->when($request->user, function ($query) use ($request) {
@@ -50,7 +64,7 @@ class RequestController extends Controller
     public function permissions(Request $request)
     {
         $permissions = Permission::query()
-            ->whereIn('user_id', auth()->user()->children()->pluck('id'))
+            ->whereIn('user_id', $this->getChildrenIds())
             ->latest()
             ->with('user')
             ->when($request->user, function ($query) use ($request) {
